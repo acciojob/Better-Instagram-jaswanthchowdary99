b072551e530456f64eabc73128190609c8432d3f
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-11-23 16:56:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
 let dragged;
 
-// events fired on the draggable target
 document.addEventListener('dragstart', function(event) {
-  // store a reference to the dragged element and the next sibling
   dragged = event.target;
 }, false);
 
-// events fired on the drop targets
 document.addEventListener('dragover', function(event) {
-  // prevent default to allow drop
   event.preventDefault();
 }, false);
 
 document.addEventListener('drop', function(event) {
-  // prevent default action
   event.preventDefault();
   if (event.target.className === 'dropzone') {
-    // swap the innerHTML of the dragged and drop target
     let tmp = event.target.innerHTML;
     event.target.innerHTML = dragged.innerHTML;
     dragged.innerHTML = tmp;
