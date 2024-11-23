2ec9533515a6d6cbc291ec26e715955eacc7d337
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-11-23 16:51:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 let dragged;
 
-document.addEventListener("dragstart", function(event) {
+// events fired on the draggable target
+document.addEventListener('dragstart', function(event) {
+  // store a reference to the dragged element and the next sibling
   dragged = event.target;
-  event.target.style.opacity = .5;
 }, false);
 
-document.addEventListener("dragend", function(event) {
-  event.target.style.opacity = "";
-}, false);
-
-document.addEventListener("dragover", function(event) {
+// events fired on the drop targets
+document.addEventListener('dragover', function(event) {
+  // prevent default to allow drop
   event.preventDefault();
 }, false);
 
-document.addEventListener("drop", function(event) {
+document.addEventListener('drop', function(event) {
+  // prevent default action
   event.preventDefault();
-  if (event.target.className == "dropzone") {
-    event.target.style.background = "";
+  if (event.target.className === 'dropzone') {
+    // swap the innerHTML of the dragged and drop target
     let tmp = event.target.innerHTML;
     event.target.innerHTML = dragged.innerHTML;
     dragged.innerHTML = tmp;
